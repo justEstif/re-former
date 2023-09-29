1696bed1dc7eed044ca53938c9fabc2c1a963f08
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-09-29 10:52:50 -0400

    feat: setup + user models + picocss

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || "is not an email")
+    end
+  end
+end
+
+class PasswordValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /[A-Z]/ && value =~ /[a-z]/ && value =~ /[0-9]/ && value =~ /[^A-Za-z0-9]/
+      record.errors.add attribute, (options[:message] || "must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+    end
+  end
+end
+
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
+  validates :email, presence: true, uniqueness: true, email: true, confirmation: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }, confirmation: true, password: true
+end
